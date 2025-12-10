5f6a07df66e326faacb1a65a051694614445d4c2
Zonghui li <zonghuili@Mac-book-pro114514.local>
2025-12-09 23:57:18 -0500

    Final project

---
+++ Final project/js/gallery.js
@@ -0,0 +1,24 @@
+
+document.getElementById("nav-toggle").onclick = function () {
+  document.getElementById("nav-links").classList.toggle("show");
+};
+
+const images = [
+  "images/gallery1.jpg",
+  "images/gallery2.jpg",
+  "images/gallery3.jpg"
+];
+
+let index = 0;
+
+function changeSlide() {
+  const slide = document.getElementById("slide");
+  index = (index + 1) % images.length;
+  slide.src = images[index];
+}
+
+setInterval(changeSlide, 3000);
+
+function toggleMenu() {
+  document.getElementById("nav-links").classList.toggle("show");
+}
